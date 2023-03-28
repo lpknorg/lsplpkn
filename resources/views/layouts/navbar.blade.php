60ebe0921285f86d9d6c75e5927145127f343faf
ari ramadhani <ari3ramadhani@gmail.com>
2023-03-28 17:05:17 +0700

    image icon

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -18,7 +18,7 @@
   <header id="header" class="d-flex align-items-center">
     <div class="container d-flex align-items-center justify-content-between">
 
-      <h1 class="logo"><a href="index.html">LSPLPKN<span>.</span></a></h1>
+      <h1 class="logo"><a href="index.html"><img src="{{ asset('images') }}/home/logo.png" alt=""></a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo"><img src="{{ asset('biz') }}/assets/img/logo.png" alt=""></a>-->
        @php
@@ -43,7 +43,7 @@
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto {{$activehome}}" href="{{route('home')}}">Home</a></li>         
+          <li><a class="nav-link scrollto {{$activehome}}" href="{{route('home')}}">Home</a></li>
           <li class="dropdown"><a href="#" class="{{$activeprofile}}"><span>Profile</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="{{route('visimisi')}}">Visi Misi</a></li>
