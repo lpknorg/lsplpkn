ae97047222e02202954e26424d387a431b30081a
Rusby <ianrusbiy02@gmail.com>
2023-03-28 16:14:14 +0700

    home

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -43,7 +43,7 @@
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto {{$activehome}}" href="#hero">Home</a></li>         
+          <li><a class="nav-link scrollto {{$activehome}}" href="{{route('home')}}">Home</a></li>         
           <li class="dropdown"><a href="#" class="{{$activeprofile}}"><span>Profile</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="{{route('visimisi')}}">Visi Misi</a></li>
