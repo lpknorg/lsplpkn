4ea42b6ff9b6834225436da39714f05485438b4a
Rusby <ianrusbiy02@gmail.com>
2023-03-29 10:45:15 +0700

    up skema

--- resources/views/skemasertifikasi.blade.php
+++ resources/views/skemasertifikasi.blade.php
@@ -3,191 +3,88 @@
 
 <section id="team" class="team section-bg">
     <div class="container aos-init aos-animate" data-aos="fade-up">
+
         <div class="section-title">
-            <h3>Skema Sertifikasi</h3>
+            <h2>Daftar</h2>
+            <h3>Skema <span>Sertifikasi</span></h3>
             <p>Lembaga Sertifikasi Profesi Lembaga Pengembangan dan Konsultasi Nasional</p>
         </div>
         <div class="back-slick">
             <div class="your-schema">
-                <div class="p-2" data-aos="fade-up" data-aos-delay="100">
+                <div class="p-1 p-md-2" data-aos="fade-up" data-aos-delay="100">
                     <div class="member">
                     <div class="member-img">
-                        <img src="{{ asset('biz') }}/assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                        <a href=""><i class="bi bi-arrow-right"></i></a>
+                        <img src="{{ asset('images') }}/home/1.png" class="img-fluid" alt="">
+                        <div class="social schema-more">
+                            <a href="{{ route('procurement', 'Procurement Staff') }}">Selengkapnya</i></a>
                         </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>Procurement Staff</h4>
-                        <span>Chief Executive Officer</span>
                     </div>
                     </div>
                 </div>
-                <div class="p-2" data-aos="fade-up" data-aos-delay="100">
+                <div class="p-1 p-md-2" data-aos="fade-up" data-aos-delay="100">
                     <div class="member">
                     <div class="member-img">
-                        <img src="{{ asset('biz') }}/assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                        <a href=""><i class="bi bi-arrow-right"></i></a>
+                        <img src="{{ asset('images') }}/home/2.png" class="img-fluid" alt="">
+                        <div class="social schema-more">
+                            <a href="{{ route('procurement',  'Procurement Senior Staff') }}">Selengkapnya</i></a>
                         </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>Procurement Staff</h4>
-                        <span>Chief Executive Officer</span>
                     </div>
                     </div>
                 </div>
-                <div class="p-2" data-aos="fade-up" data-aos-delay="100">
+                <div class="p-1 p-md-2" data-aos="fade-up" data-aos-delay="100">
                     <div class="member">
                     <div class="member-img">
-                        <img src="{{ asset('biz') }}/assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                        <a href=""><i class="bi bi-arrow-right"></i></a>
+                        <img src="{{ asset('images') }}/home/3.png" class="img-fluid" alt="">
+                        <div class="social schema-more">
+                            <a href="{{ route('procurement',  'Procurement Coordinator') }}">Selengkapnya</i></a>
                         </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>Procurement Staff</h4>
-                        <span>Chief Executive Officer</span>
                     </div>
                     </div>
                 </div>
-                <div class="p-2" data-aos="fade-up" data-aos-delay="100">
+                <div class="p-1 p-md-2" data-aos="fade-up" data-aos-delay="100">
                     <div class="member">
                     <div class="member-img">
-                        <img src="{{ asset('biz') }}/assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                        <a href=""><i class="bi bi-arrow-right"></i></a>
+                        <img src="{{ asset('images') }}/home/4.png" class="img-fluid" alt="">
+                        <div class="social schema-more">
+                            <a href="{{ route('procurement',  'Procurement Supervisor') }}">Selengkapnya</i></a>
                         </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>Procurement Staff</h4>
-                        <span>Chief Executive Officer</span>
                     </div>
                     </div>
                 </div>
-
-                <div class="p-2" data-aos="fade-up" data-aos-delay="100">
+                <div class="p-1 p-md-2" data-aos="fade-up" data-aos-delay="100">
                     <div class="member">
                     <div class="member-img">
-                        <img src="{{ asset('biz') }}/assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                        <a href=""><i class="bi bi-arrow-right"></i></a>
+                        <img src="{{ asset('images') }}/home/5.png" class="img-fluid" alt="">
+                        <div class="social schema-more">
+                            <a href="{{ route('procurement',  'Procurement Asistant Manager') }}">Selengkapnya</i></a>
                         </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>Procurement Staff</h4>
-                        <span>Chief Executive Officer</span>
                     </div>
                     </div>
                 </div>
-                <div class="p-2" data-aos="fade-up" data-aos-delay="100">
+                <div class="p-1 p-md-2" data-aos="fade-up" data-aos-delay="100">
                     <div class="member">
                     <div class="member-img">
-                        <img src="{{ asset('biz') }}/assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                        <a href=""><i class="bi bi-arrow-right"></i></a>
+                        <img src="{{ asset('images') }}/home/6.png" class="img-fluid" alt="">
+                        <div class="social schema-more">
+                            <a href="{{ route('procurement',  'Procurement Manager') }}">Selengkapnya</i></a>
                         </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>Procurement Staff</h4>
-                        <span>Chief Executive Officer</span>
-                    </div>
-                    </div>
-                </div>
-                <div class="p-2" data-aos="fade-up" data-aos-delay="100">
-                    <div class="member">
-                    <div class="member-img">
-                        <img src="{{ asset('biz') }}/assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                        <a href=""><i class="bi bi-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>Procurement Staff</h4>
-                        <span>Chief Executive Officer</span>
-                    </div>
-                    </div>
-                </div>
-                <div class="p-2" data-aos="fade-up" data-aos-delay="100">
-                    <div class="member">
-                    <div class="member-img">
-                        <img src="{{ asset('biz') }}/assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                        <a href=""><i class="bi bi-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>Procurement Staff</h4>
-                        <span>Chief Executive Officer</span>
                     </div>
                     </div>
                 </div>
 
-                <div class="p-2" data-aos="fade-up" data-aos-delay="100">
-                    <div class="member">
-                    <div class="member-img">
-                        <img src="{{ asset('biz') }}/assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                        <a href=""><i class="bi bi-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>Procurement Staff</h4>
-                        <span>Chief Executive Officer</span>
-                    </div>
-                    </div>
-                </div>
-                <div class="p-2" data-aos="fade-up" data-aos-delay="100">
-                    <div class="member">
-                    <div class="member-img">
-                        <img src="{{ asset('biz') }}/assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                        <a href=""><i class="bi bi-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>Procurement Staff</h4>
-                        <span>Chief Executive Officer</span>
-                    </div>
-                    </div>
-                </div>
-                <div class="p-2" data-aos="fade-up" data-aos-delay="100">
-                    <div class="member">
-                    <div class="member-img">
-                        <img src="{{ asset('biz') }}/assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                        <a href=""><i class="bi bi-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>Procurement Staff</h4>
-                        <span>Chief Executive Officer</span>
-                    </div>
-                    </div>
-                </div>
-                <div class="p-2" data-aos="fade-up" data-aos-delay="100">
-                    <div class="member">
-                    <div class="member-img">
-                        <img src="{{ asset('biz') }}/assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                        <a href=""><i class="bi bi-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>Procurement Staff</h4>
-                        <span>Chief Executive Officer</span>
-                    </div>
-                    </div>
-                </div>
 
             </div>
-            <div class="schema-prev">
+            <div class="schema-prev desktop">
                 <span><i class="fa-solid fa-angle-left"></i></span>
             </div>
-            <div class="schema-next">
+            <div class="schema-next desktop">
                 <span><i class="fa-solid fa-angle-right"></i></span>
             </div>
         </div>
+
+
+
+
     </div>
 </section>
 
